7b88af53b2f13eb1cff146768aab19bccb2a493f
Emanuel Leite <emanuel.leite10@outlook.com>
2024-03-02 20:51:40 -0600

    Adding Button Component

---
+++ src/components/Button.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string;
+  format?: 'small' | 'default' | 'largeSize';
+  color?: 'black' | 'white' | 'blue';
+}>();
+
+const format = props.format ? props.format : 'small';
+const color = props.color ? props.color : 'black';
+const textColor = props.color === 'white' ? 'black' : '#FFFFFF'
+
+const buttonsColor = {
+    black: 'bg-primary2',
+    white: 'bg-secondary2',
+    blue: 'bg-primary1'
+}
+
+const buttonStyle = {
+    small: {
+        horizontalPadding: "px-12",
+    },
+    default: {
+        horizontalPadding: "px-20",
+    },
+    largeSize: {
+        horizontalPadding: "px-[60px]",
+    },
+};
+
+</script>
+
+<template>
+    <button class="bg-primary2 text-[18px] font-semibold py-5 rounded-full"
+        :class="buttonStyle[format].horizontalPadding, buttonsColor[color], `text-[${textColor}]`">
+        {{ props.text }}
+    </button>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary1: "#5956E9",
+      primary2: "#2F2F2F",
+      secondary1: "#F6F6F6",
+      secondary2: "#FFFFFF",
+      secondary3: "#797979", 
+    },
     extend: {},
   },
   plugins: [],
